84b3c236d8d6b57e8aac6eb26b157953ead8b73f
yotakih <37173608+yotakih@users.noreply.github.com>
2018-03-28 19:14:59 +0900

    init commit

---
+++ ComputeHash.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace ComputeHashSelectFileLib
+{
+	public class ComputeHash
+	{
+		private static MD5CryptoServiceProvider md5Provider = new MD5CryptoServiceProvider();
+
+		private ComputeHash ()
+		{
+		}
+
+		public static string getHashWithFile(string path)
+		{
+			using (var fs = new FileStream (path, FileMode.Open)) {
+				byte[] b = md5Provider.ComputeHash (fs);
+				return BitConverter.ToString (b).ToLower().Replace (@"-", @"");
+			}
+		}
+	}
+}
+
---
+++ CustomFileInfo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace ComputeHashSelectFileLib
+{
+	public class CustomFileInfo
+	{
+		private string _filePath;
+
+		public CustomFileInfo (string path)
+		{
+			this._filePath = path;
+		}
+
+		public string path {
+			get {
+				return this._filePath;
+			}
+		}
+
+		public string fileName {
+			get {
+				return File.Exists(_filePath) ? Path.GetFileName (this.path) : @"FILE NOT FOUND";
+			}
+		}
+
+		public string lastWriteTime {
+			get {
+				return File.Exists(_filePath) ? File.GetLastWriteTime (this.path).ToString() : @"FILE NOT FOUND";
+			}
+		}
+	
+		public string hashOfFileContent {
+			get {
+				return File.Exists(_filePath) ? ComputeHash.getHashWithFile (this.path): @"FILE NOT FOUND";
+			}
+		}
+	}
+}
+
